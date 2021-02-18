901ef2d324b183d286bbcdd14f2f7903a7717fb3
ArnavGupta12 <75476460+ArnavGupta12@users.noreply.github.com>
2021-02-18 15:50:15 +0530

    Add files via upload

---
+++ umbrella.js
@@ -0,0 +1,27 @@
+
+
+
+  
+class Umbrella {
+    constructor(x, y, width, height) {
+
+    var options ={
+        isStatic: true,
+        restitution: 0.8
+    }
+
+  this.body = Bodies.rectangle(x, y, width, height, options);
+  this.width = width * 2;
+  this.height = height * 2;
+  
+  this.image = loadImage("images/Walking Frame/walking_1.png");
+  World.add(world, this.body);
+}
+display() {
+    var pos = this.body.position;
+    imageMode(CENTER);
+    fill("white");
+    image(this.image, pos.x, pos.y, this.width, this.height);
+}
+
+}
